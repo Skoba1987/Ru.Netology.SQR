78431e09bba946f345174701246a00f2e7acf8f2
Skoba Kristina <105549745+Skoba1987@users.noreply.github.com>
2022-08-29 16:35:37 +0300

    Update StatsServiceTest.java

--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -3,22 +3,64 @@ package ru.netology.stats;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+
 public class StatsServiceTest {
+
     @Test
-    public void test() {
+    public void testGetSumSales() {
         StatsService service = new StatsService();
-        long[]sales = {8,15,13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-        long actual = service.sum(sales);
-        long expected = 8+15+13+15+17+20+19+20+7+14+14+18;
+        int[] allSales = {1100, 2350, 3445, 1680, 2370, 3200, 1225, 2525, 3115, 1785, 2290, 3420};
+
+        int expected = 28505;
+        int actual = service.getSumSales(allSales);
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
-            public void test () {
+    public void testGetMiddleSales() {
         StatsService service = new StatsService();
-        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-        int actual = service.countBelowAvg(sales);
-        int expected = 15;
+        int[] allSales = {1100, 2350, 3445, 1680, 2370, 3200, 1225, 2525, 3115, 1785, 2290, 3420};
+
+        int expected = 2375;
+        int actual = service.getMiddleSales(allSales);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetMaxSalesMonth() {
+        StatsService service = new StatsService();
+        int[] allSales = {1100, 2350, 3445, 1680, 2370, 3200, 1225, 2525, 3115, 1785, 2290, 3420};
+
+        int expected = 2;
+        int actual = service.getMaxSalesMonth(allSales);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetMinSalesMonth() {
+        StatsService service = new StatsService();
+        int[] allSales = {1100, 2350, 3445, 1680, 2370, 3200, 1225, 2525, 3115, 1785, 2290, 3420};
+
+        int expected = 0;
+        int actual = service.getMinSalesMonth(allSales);
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void testGetMonthDown() {
+        StatsService service = new StatsService();
+        int[] allSales = {1100, 2350, 3445, 1680, 2370, 3200, 1225, 2525, 3115, 1785, 2290, 3420};
+
+        int expected = 7;
+        int actual = service.getMonthDown(allSales);
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void testGetMonthHigh() {
+        StatsService service = new StatsService();
+        int[] allSales = {1100, 2350, 3445, 1680, 2370, 3200, 1225, 2525, 3115, 1785, 2290, 3420};
+
+        int expected = 5;
+        int actual = service.getMonthHigh(allSales);
         Assertions.assertEquals(expected, actual);
     }
 
